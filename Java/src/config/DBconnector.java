4e1d5f14a8c16612df07a8d2dd7099208972a226
niv3D <niv3d.username@gmail.com>
2023-05-04 13:40:51 +0530

    project-structure

--- Java/src/config/DBconnector.java
+++ Java/src/config/DBconnector.java
@@ -1,5 +1,19 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBconnector {
 
+	private DBconnector() {
+	}
+
+	private static String url = "jdbc:mysql://localhost:3306/InventoryDB";
+	private static String user = "root";
+	private static String password = "root@mysql";
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
 }
