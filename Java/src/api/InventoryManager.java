315221eb3f9271f46e2ec8d7ae6a8b2182b7b6f0
belbin07 <84174870+belbin07@users.noreply.github.com>
2023-05-07 17:25:31 +0530

    Update InventoryManager.java

--- Java/src/api/InventoryManager.java
+++ Java/src/api/InventoryManager.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
 
 import config.DBconnector;
 import models.InventoryRecord;
@@ -19,10 +21,16 @@ public class InventoryManager {
 	 * insert new inventory record in the database.
 	 * 
 	 * @param inventory <code>models.inventoryRecords</code> object
-	 * @return id of the record
+	 * @return id of the record or null if low stock
 	 */
-	public static int updateStock(InventoryRecord inventory) {
-
+	public static Integer updateStock(InventoryRecord inventory) {
+		
+		int totalStock = getProductQuantity(inventory.productId);
+		
+		if((totalStock + inventory.quantity)<0) {
+			return null;
+		}
+		
 		int id = 0;
 		ResultSet resultSet = null;
 
@@ -69,6 +77,58 @@ public class InventoryManager {
 		return id;
 	}
 	
+	/**
+	 * To get quantity of individual product in the database.
+	 * 
+	 * @param productId
+	 * @return quantity of the product 
+	 */
+
+	public static int getProductQuantity(int productId) {
+		int sum = 0;
+
+		String sqlString = "SELECT SUM(quantity) FROM inventoryrecords WHERE product_id =" + productId;
+
+		try (Connection connection = DBconnector.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sqlString);
+				) {
+			
+			if(resultSet.next()) {
+				sum=resultSet.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return sum;
+
+	}
 	
+	/**
+	 * To get all available stock of all products in the database.
+	 *  @return all the product id and available stock 
+	 */
+	
+	public static List<String> productStock () {
+		List<String> stock=new ArrayList<>();
+		String sqlString="SELECT p.id, name, SUM(i.quantity) FROM products p INNER JOIN inventoryrecords i ON p.id = i.product_id GROUP BY p.id ORDER BY p.id;";
+		try (Connection connection = DBconnector.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sqlString);
+				) {
+			
+			while(resultSet.next()) {
+				stock.add(Integer.toString(resultSet.getInt(1))+" "+Integer.toString(resultSet.getInt(2)));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return stock;
+		
+	}
 
 }
