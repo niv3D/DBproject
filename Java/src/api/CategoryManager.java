74b2d76aa62eeef1172801e42b7e0fe1d0d27eb8
belbin07 <84174870+belbin07@users.noreply.github.com>
2023-05-07 11:50:03 +0530

    Update CategoryManager.java

--- Java/src/api/CategoryManager.java
+++ Java/src/api/CategoryManager.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import config.DBconnector;
 
@@ -17,7 +19,7 @@ public class CategoryManager {
 	/**
 	 * inserts a new category in the database.
 	 * 
-	 * @param name of the category
+	 * @parameter name of the category
 	 * @return generated category id or 0 if unsuccessful
 	 */
 	public static int insert(String name) {
@@ -69,8 +71,8 @@ public class CategoryManager {
 	/**
 	 * updates a category in the database.
 	 * 
-	 * @param id   of the category
-	 * @param name for updating
+	 * @parameter id of the category
+	 * @parameter name for updating
 	 * @return either 1 if successful or 0 if unsuccessful
 	 */
 	public static int update(int id, String name) {
@@ -99,12 +101,92 @@ public class CategoryManager {
 
 	}
 
-	public static int delete() {
-		int rowAffected = 0;
+	/**
+	 * deletes a category in the database.
+	 * 
+	 * @parameter id of the category
+	 *
+	 * @return either 1 if successful or 0 if unsuccessful
+	 */
 
-		// TO-DO
+	public static int delete(int id) {
+
+		int rowAffected = 0;
+		String sqlString = "DELETE FROM Categories WHERE id= ?";
+
+		try (Connection connection = DBconnector.getConnection();
+				PreparedStatement statement = connection.prepareStatement(sqlString);) {
+
+			statement.setInt(1, id);
+			rowAffected = statement.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		return rowAffected;
+
+	}
+
+	/**
+	 * search for categories with name in database.
+	 * 
+	 * @parameter id
+	 * @return a <code>List</code> of <code>categories</code>
+	 */
+
+	public static List<String> search(int id) {
+
+		List<String> categories = new ArrayList<>();
+
+		String sqlString = "SELECT id,name FROM categories WHERE id=" + id;
+
+		try (Connection connection = DBconnector.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sqlString);) {
+
+			while (resultSet.next()) {
+				categories.add(Integer.toString(resultSet.getInt(1)) + " " + resultSet.getString(2));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return categories;
+
+	}
+
+	/**
+	 * search for categories with name in database.
+	 * 
+	 * @parameter category name
+	 * @return a <code>List</code> of <code>categories</code>
+	 */
+	public static List<String> search(String name) {
+
+		List<String> categories = new ArrayList<>();
+
+		if (" ".equals(name)) {
+			return categories;
+		}
+
+		String sqlString = "SELECT id,name FROM categories WHERE name LIKE '%" + name + "%'";
+
+		try (Connection connection = DBconnector.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sqlString);) {
+
+			while (resultSet.next()) {
+				categories.add(Integer.toString(resultSet.getInt(1)) + " " + resultSet.getString(2));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return categories;
+
 	}
 
 }
